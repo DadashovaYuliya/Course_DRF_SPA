f717cb30c0167c321fe68a5546219c7c26ecca71
Yuliya_Dadashova <dya0305@yandex.ru>
2025-06-10 16:48:31 +0300

    red settings

--- config/settings.py
+++ config/settings.py
@@ -189,3 +189,11 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": timedelta(minutes=10),  # Расписание выполнения задачи
     },
 }
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db.sqlite3"
+        }
+    }
